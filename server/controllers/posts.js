96be0dc75d993f71e6ea53641a4b41c10320ca8f
Anshul Verma <anshulv0111@gmail.com>
2022-02-06 20:42:14 +0530

    Initital set up for client and server

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,7 +3,6 @@ import PostMessage from "../models/postMessage.js"
 export const getPosts = async (req,res) => {
 
     try {
-
         const postMessages = await PostMessage.find()
         res.status(200).json(postMessages)
 
